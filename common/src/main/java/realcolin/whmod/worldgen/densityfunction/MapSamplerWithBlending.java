4ba494579f1fdbcb0de07fbd5b66f3e8202ba370
Werner, Colin <crwgond@gmail.com>
2026-01-21 21:27:45 -0600

    Blending MOSTLY works

--- common/src/main/java/realcolin/whmod/worldgen/densityfunction/MapSamplerWithBlending.java
+++ common/src/main/java/realcolin/whmod/worldgen/densityfunction/MapSamplerWithBlending.java
@@ -41,50 +41,52 @@ public class MapSamplerWithBlending implements DensityFunction.SimpleFunction {
 
     @Override
     public double compute(FunctionContext fnc) {
-        var p = new Pair(fnc.blockX(), fnc.blockZ());
-        if (cache.containsKey(p))
-            return cache.get(p);
-
-        var cell = map.value().getCellAt(p.a(), p.b());
-        var colors = cell.getDistTransforms().keySet();
-
-        var nearbyRegions = new ArrayList<RegionWeight>();
-
-        for (var c : colors) {
-            var arr = cell.getDistTransforms().get(c);
-            int ox = Math.floorMod(p.a(), Constants.CELL_SIZE) + (Constants.CELL_BUFFER / 2);
-            int oz = Math.floorMod(p.b(), Constants.CELL_SIZE) + (Constants.CELL_BUFFER / 2);
-            var dist = arr[ox][oz];
-
-            var terrain = map.value().getTerrainFromColor(c);
-            var range = Math.min(terrain.blendRange(), Constants.BLEND_RANGE);
-
-            if (dist <= range) {
-                var weight = calculateWeight(dist, range);
-                var finalWeight = weight * terrain.blendWeight();
-                nearbyRegions.add(new RegionWeight(terrain, dist, finalWeight));
-            }
-        }
-
-        if (nearbyRegions.size() == 1) {
-            var ter = nearbyRegions.getFirst().terrain();
-            var val = functions.getOrDefault(ter, field.read(ter)).compute(fnc);
-            cache.put(p, val);
-            return val;
-        }
-
-        var totalWeight = nearbyRegions.stream().mapToDouble(RegionWeight::weight).sum();
-        var blendedValue = 0.0;
-
-        for (var region : nearbyRegions) {
-            var ter = region.terrain();
-            var func = functions.getOrDefault(ter, field.read(ter));
-            var val = func.compute(fnc);
-            blendedValue += val * (region.weight() / totalWeight);
-        }
-
-        cache.put(p, blendedValue);
-        return blendedValue;
+        return newCompute(fnc);
+//
+//        var p = new Pair(fnc.blockX(), fnc.blockZ());
+//        if (cache.containsKey(p))
+//            return cache.get(p);
+//
+//        var cell = map.value().getCellAt(p.a(), p.b());
+//        var colors = cell.getDistTransforms().keySet();
+//
+//        var nearbyRegions = new ArrayList<RegionWeight>();
+//
+//        for (var c : colors) {
+//            var arr = cell.getDistTransforms().get(c);
+//            int ox = Math.floorMod(p.a(), Constants.CELL_SIZE) + (Constants.CELL_BUFFER / 2);
+//            int oz = Math.floorMod(p.b(), Constants.CELL_SIZE) + (Constants.CELL_BUFFER / 2);
+//            var dist = arr[ox][oz];
+//
+//            var terrain = map.value().getTerrainFromColor(c);
+//            var range = Math.min(terrain.blendRange(), Constants.BLEND_RANGE);
+//
+//            if (dist <= range) {
+//                var weight = calculateWeight(dist, range);
+//                var finalWeight = weight * terrain.blendWeight();
+//                nearbyRegions.add(new RegionWeight(terrain, dist, finalWeight));
+//            }
+//        }
+//
+//        if (nearbyRegions.size() == 1) {
+//            var ter = nearbyRegions.getFirst().terrain();
+//            var val = functions.getOrDefault(ter, field.read(ter)).compute(fnc);
+//            cache.put(p, val);
+//            return val;
+//        }
+//
+//        var totalWeight = nearbyRegions.stream().mapToDouble(RegionWeight::weight).sum();
+//        var blendedValue = 0.0;
+//
+//        for (var region : nearbyRegions) {
+//            var ter = region.terrain();
+//            var func = functions.getOrDefault(ter, field.read(ter));
+//            var val = func.compute(fnc);
+//            blendedValue += val * (region.weight() / totalWeight);
+//        }
+//
+//        cache.put(p, blendedValue);
+//        return blendedValue;
     }
 
     @Override
@@ -110,6 +112,100 @@ public class MapSamplerWithBlending implements DensityFunction.SimpleFunction {
         return v.apply(new MapSamplerWithBlending(map, field, tmpFuncs));
     }
 
+    @Override
+    public @NotNull KeyDispatchDataCodec<? extends DensityFunction> codec() {
+        return new KeyDispatchDataCodec<>(CODEC);
+    }
+
+    public double newCompute(FunctionContext fnc) {
+        var p = new Pair(fnc.blockX(), fnc.blockZ());
+        if (cache.containsKey(p))
+            return cache.get(p);
+
+        var worldMap = map.value();
+        var cell = worldMap.getCellAt(p.a(), p.b());
+        var currentTerrain = worldMap.getTerrainFromColor(cell.getColorAt(p.a(), p.b()));
+        var colors = cell.getDistTransforms().keySet();
+
+        var landRegions = new ArrayList<RegionWeight>();
+        var waterRegions = new ArrayList<RegionWeight>();
+
+        for (var c : colors) {
+            var arr = cell.getDistTransforms().get(c);
+            int ox = Math.floorMod(p.a(), Constants.CELL_SIZE) + (Constants.CELL_BUFFER / 2);
+            int oz = Math.floorMod(p.b(), Constants.CELL_SIZE) + (Constants.CELL_BUFFER / 2);
+            var dist = arr[ox][oz];
+
+            var terrain = worldMap.getTerrainFromColor(c);
+            var range = Math.min(terrain.blendRange(), Constants.BLEND_RANGE);
+
+            if (dist <= range) {
+                var weight = calculateWeight(dist, range);
+                var finalWeight = weight * terrain.blendWeight();
+                var regionWeight = new RegionWeight(terrain, dist, finalWeight);
+                if (terrain.isWater())
+                    waterRegions.add(regionWeight);
+                else
+                    landRegions.add(regionWeight);
+            }
+        }
+
+        var landWeight = landRegions.stream().mapToDouble(RegionWeight::weight).sum();
+        var waterWeight = waterRegions.stream().mapToDouble(RegionWeight::weight).sum();
+        var blendedValue = 0.0;
+
+
+        if (!currentTerrain.isWater()) {
+            // apply weights from land regions
+            for (var region : landRegions) {
+                var ter = region.terrain();
+                var func = functions.getOrDefault(ter, field.read(ter));
+                var val = func.compute(fnc);
+                blendedValue += val * (region.weight() / landWeight);
+            }
+
+            // get distance to nearest water region
+            var distToWater = Constants.BLEND_RANGE;
+            RegionWeight weight = null;
+
+            for (var region : waterRegions) {
+                if (region.distance() < distToWater) {
+                    distToWater = region.distance();
+                    weight = region;
+                }
+//                distToWater = Math.min(distToWater, region.distance());
+            }
+
+            var d = Constants.BLEND_RANGE;
+            if (weight != null) {
+                d = weight.terrain().blendRange();
+            }
+
+            // blend height from h down to 0.0 from d=50 to d=0
+            blendedValue = lerp(distToWater, d, 0.0, blendedValue, 0.0);
+
+        } else {
+            // do the same thing but vice versa
+            for (var region : waterRegions) {
+                var ter = region.terrain();
+                var func = functions.getOrDefault(ter, field.read(ter));
+                var val = func.compute(fnc);
+                blendedValue += val * (region.weight() / waterWeight);
+            }
+
+            var distToLand = Constants.BLEND_RANGE;
+            for (var region : landRegions) {
+                distToLand = Math.min(distToLand, region.distance());
+            }
+
+
+            blendedValue = lerp(distToLand, Constants.BLEND_RANGE, 0.0, blendedValue, 0.0);
+        }
+
+        cache.put(p, blendedValue);
+        return blendedValue;
+    }
+
     private double calculateWeight(double distance, double range) {
         if (distance == 0) return 1.0;
         if (distance >= range) return 0.0;
@@ -122,9 +218,8 @@ public class MapSamplerWithBlending implements DensityFunction.SimpleFunction {
         return x * x * (3.0 - 2.0 * x);
     }
 
-    @Override
-    public @NotNull KeyDispatchDataCodec<? extends DensityFunction> codec() {
-        return new KeyDispatchDataCodec<>(CODEC);
+    private double lerp(double val,  double A, double B, double C, double D) {
+        return C + ((val - A) / (B - A)) * (D - C);
     }
 
     record RegionWeight(Terrain terrain, double distance, double weight) {}
